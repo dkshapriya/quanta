732973cd570952ff0935ec4d75ade97cb50fadd5
dkshapriya <78372837+dkshapriya@users.noreply.github.com>
2021-03-12 11:41:46 +0530

    Add files via upload

---
+++ java/src/LoginCheck.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/LoginCheck")
+public class LoginCheck extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    public LoginCheck() {
+        super();
+        }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uname=request.getParameter("uname");
+		String password=request.getParameter("password");
+		if(uname.equals("diksha") && password.equals("1234"))
+		{
+			response.sendRedirect("user.jsp");
+		}
+		else
+		{
+			response.sendRedirect("error.jsp");
+		}
+	}
+
+}
